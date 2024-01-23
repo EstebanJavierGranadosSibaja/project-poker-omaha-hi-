18da08d9e6d8c4a5c8177365ba0526828ed628ee
Esteban <esteban.granados.sibaja@est.una.ac.cr>
2024-01-23 14:46:22 -0600

    test commit

--- Classes/dealer.cpp
+++ Classes/dealer.cpp
@@ -1,1 +1,46 @@
 #include"dealer.h"
+
+void Dealer::shuffleDeck(Deck* newDeck)
+{
+    int startDeck = 0;
+	int maxPosicion = 52;
+	int randomPosicion;
+	srand(time(NULL));
+
+    for (int i = startDeck; i < maxPosicion; i++) 
+    {
+		maxPosicion -= startDeck;
+		randomPosicion = maxPosicion + rand() % startDeck;
+
+		Deck tempDeck = newDeck[i];
+		newDeck[i] = newDeck[randomPosicion];
+		newDeck[randomPosicion] = tempDeck;
+
+		startDeck += 1;
+    }
+}
+
+PlayerHand** Dealer::getPlayerCards(PlayerHand** newHand)
+{
+	return nullptr;
+}
+
+Card** Dealer::getFloat()
+{
+	return nullptr;
+}
+
+Card** Dealer::getTurn()
+{
+	return nullptr;
+}
+
+Card** Dealer::getRiver()
+{
+	return nullptr;
+}
+
+Card** Dealer::getShowedCards()
+{
+	return nullptr;
+}
--- Classes/dealer.h
+++ Classes/dealer.h
@@ -1,9 +1,18 @@
 #pragma once
+#include "deck.h"
+#include "player_hand.h"
 
 class Dealer
 {
 private:
 
 public:
+	void shuffleDeck(Deck* newDeck);
+	PlayerHand** getPlayerCards(PlayerHand** newHand);
+	Card** getFloat();
+	Card** getTurn();
+	Card** getRiver();
+	Card** getShowedCards();
+
 
 }; 
