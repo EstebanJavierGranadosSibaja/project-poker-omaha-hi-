733c06d82ccf7c9789c6eb2b4278b765e2015fc9
Esteban <esteban.granados.sibaja@est.una.ac.cr>
2024-01-26 22:43:05 -0600

    Development Class RoyalFlush

--- poker_omaha_hi/royal_flush.cpp
+++ poker_omaha_hi/royal_flush.cpp
@@ -1,37 +1,137 @@
 #include "royal_flush.h"
 
-int RoyalFlush::getPlayeRanking(Card** newHand, Card** newCommunityCards)
+RoyalFlush::RoyalFlush()
 {
-	
+	for (int i = 0; i < NUMBER_OF_ROYAL_FLUSH_VALUES; i++)
+	{
+		clubsRoyalFlush[i] = 'X';
+		pikesRoyalFlush[i] = 'X';
+		heartskRoyalFlush[i] = 'X';
+		diamondsRoyalFlush[i] = 'X';
+	}
+}
 
+// terminar el metodo principal de validadcion,
+int RoyalFlush::getPlayerRanking(Card** newHand, Card** newCommunityCards)
+{
 	return 0;
 }
 
-bool RoyalFlush::isValidCommunityHand(Card** newCommunityCards)
+void RoyalFlush::getClubsRoyalFlush(Card** newHand, Card** newCommunityCards)
 {
+	int clubsPosition = 0;
+	int numberOfType = 2;
+
 	for (int i = 0; i < NUMBER_OF_VALID_COMMUNITY_HAND; i++)
 	{
-		for (int j = 0; j < NUMBER_OF_TYPES; j++)
+		if (checkRoyalFlush(newCommunityCards[i], clubsPosition, numberOfType))
 		{
-		
+			clubsPosition += 1;
+			break;
 		}
 	}
 
-
-	return false;
-}
-
-bool RoyalFlush::isValidPlayerHand(Card** newHand)
-{
-	return false;
-}
-
-bool RoyalFlush::checkCommunityHandPosition(Card** newCommunityCards, string newType)
-{
-	if (newCommunityCards == newType)
+	for (int i = 0; i < NUMBER_OF_VALID_PLAYER_HAND; i++)
 	{
+		if (checkRoyalFlush(newCommunityCards[i], clubsPosition, numberOfType))
+		{
+			clubsPosition += 1;
+			break;
+		}
+	}
+}
 
+void RoyalFlush::getPikesRoyalFlush(Card** newHand, Card** newCommunityCards)
+{
+	int pikesPosition = 0;
+	int numberOfType = 3;
+
+	for (int i = 0; i < NUMBER_OF_VALID_COMMUNITY_HAND; i++)
+	{
+		if (checkRoyalFlush(newCommunityCards[i], pikesPosition, numberOfType))
+		{
+			pikesPosition += 1;
+			break;
+		}
+	}
+
+	for (int i = 0; i < NUMBER_OF_VALID_PLAYER_HAND; i++)
+	{
+		if (checkRoyalFlush(newCommunityCards[i], pikesPosition, numberOfType))
+		{
+			pikesPosition += 1;
+			break;
+		}
+	}
+}
+
+void RoyalFlush::getHeartsRoyalFlush(Card** newHand, Card** newCommunityCards)
+{
+	int heartsPosition = 0;
+	int numberOfType = 0;
+
+	for (int i = 0; i < NUMBER_OF_VALID_COMMUNITY_HAND; i++)
+	{
+		if (checkRoyalFlush(newCommunityCards[i], heartsPosition, numberOfType))
+		{
+			heartsPosition += 1;
+			break;
+		}
+	}
+
+	for (int i = 0; i < NUMBER_OF_VALID_PLAYER_HAND; i++)
+	{
+		if (checkRoyalFlush(newCommunityCards[i], heartsPosition, numberOfType))
+		{
+			heartsPosition += 1;
+			break;
+		}
+	}
+}
+
+void RoyalFlush::getDiamondsRoyalFlush(Card** newHand, Card** newCommunityCards)
+{
+	int diamondsPosition = 0;
+	int numberOfType = 3;
+
+	for (int i = 0; i < NUMBER_OF_VALID_COMMUNITY_HAND; i++)
+	{
+		if (checkRoyalFlush(newCommunityCards[i], diamondsPosition, numberOfType))
+		{
+			diamondsPosition += 1;
+			break;
+		}
+	}
+
+	for (int i = 0; i < NUMBER_OF_VALID_PLAYER_HAND; i++)
+	{
+		if (checkRoyalFlush(newCommunityCards[i], diamondsPosition, numberOfType))
+		{
+			diamondsPosition += 1;
+			break;
+		}
+	}
+}
+
+// ver si se puede hacer con menos parametros
+bool RoyalFlush::checkRoyalFlush(Card* newCard, int newPosition, int newNumberOfType)
+{
+	bool isPikes = newCard->getType() = ARRAY_OF_TYPES[newNumberOfType];
+
+	for (int i = 0; i < NUMBER_OF_ROYAL_FLUSH_VALUES; i++)
+	{
+		bool isSameValue = newCard->getValue() == ROYAL_FLUSH_VALUES[i];
+
+		if (isSameValue && isPikes)
+		{
+			//Buscar como refactorizar para que sea un metodo universal
+			vectorRoyalFlush[newPosition] = ROYAL_FLUSH_VALUES[i];
+			return true;
+		}
 	}
 
 	return false;
 }
+
+
+
--- poker_omaha_hi/royal_flush.h
+++ poker_omaha_hi/royal_flush.h
@@ -3,21 +3,27 @@
 
 const int NUMBER_OF_ROYAL_FLUSH_TYPES = 4;
 const int NUMBER_OF_ROYAL_FLUSH_VALUES = 5;
-const string ROYAL_FLUSH_TYPES[NUMBER_OF_ROYAL_FLUSH_TYPES] = { "Hearts", "Diamonds", "Clubs", "Pikes" };
 const char ROYAL_FLUSH_VALUES[NUMBER_OF_ROYAL_FLUSH_VALUES] = { 'A','J','Q','K','10' };
 
 class RoyalFlush : public WinningPlay
 {
 private:
+	char clubsRoyalFlush[NUMBER_OF_ROYAL_FLUSH_VALUES];
+	char pikesRoyalFlush[NUMBER_OF_ROYAL_FLUSH_VALUES];
+	char heartskRoyalFlush[NUMBER_OF_ROYAL_FLUSH_VALUES];
+	char diamondsRoyalFlush[NUMBER_OF_ROYAL_FLUSH_VALUES];
 
 public:
 	RoyalFlush();
 
-	int getPlayeRanking(Card** newHand, Card** newCommunityCards);
-	bool isValidCommunityHand(Card** newCommunityCards);
-	bool isValidPlayerHand(Card** newHand);
-	bool checkCommunityHandPosition(Card** newCommunityCards, string newType);
-	bool checkPlayerHandPosition(Card** newHand, char newValue);
+	int getPlayerRanking(Card** newHand, Card** newCommunityCards);
+
+	void getClubsRoyalFlush(Card** newHand, Card** newCommunityCards);
+	void getPikesRoyalFlush(Card** newHand, Card** newCommunityCards);
+	void getHeartsRoyalFlush(Card** newHand, Card** newCommunityCards);
+	void getDiamondsRoyalFlush(Card** newHand, Card** newCommunityCards);
+
+	bool checkRoyalFlush(Card* newCard, int newPikesPosition, int newNumberOfType);
 
 };
 
