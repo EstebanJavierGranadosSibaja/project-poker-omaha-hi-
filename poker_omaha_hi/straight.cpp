c6d9b61c0a9050866f8a1a8652f0e6f38b2c81ba
Jazmin Gamboa Chacon <jazmin.gamboa.chacon@est.una.ac.cr>
2024-01-28 22:39:05 -0600

    Add isACheckStraight(Card** vectorOfCombinations, int startOfTheStaircase)

---
+++ poker_omaha_hi/straight.cpp
@@ -0,0 +1,116 @@
+#include"straight.h"
+
+int Straight::getPlayerHandRankin(Card** newHand, Card** newCommunityCards)
+{
+	int firstCardUser = 0;
+	int secondCardUser = 1;
+
+	Card** combinationOfCards = new Card * [SIZE_OF_COMMUNITY_CARDS];
+
+	while (firstCardUser != NUMBER_OF_VALID_COMMUNITY_HAND)
+	{
+
+		combinationOfCards[0] = newHand[firstCardUser];
+		combinationOfCards[1] = newHand[secondCardUser];
+
+		if (communityCardCombinations(combinationOfCards, newCommunityCards)) {
+			return STRAIGHT_VALUE;
+		}
+
+		secondCardUser++;
+		if (secondCardUser == SIZE_OF_PLAYER_DECK)
+		{
+			firstCardUser++;
+			secondCardUser = firstCardUser + 1;
+		}
+	}
+	return -1;
+}
+
+bool Straight::communityCardCombinations(Card** combinationOfCards, Card** newCommunityCards)
+{
+	int firstCardCommunity = 0;
+	int secondCardCommunity = 1;
+	int thirdCardCommunity = secondCardCommunity + 1;
+
+	for (firstCardCommunity = 0; firstCardCommunity < NUMBER_OF_VALID_COMMUNITY_HAND; firstCardCommunity++)
+	{
+		secondCardCommunity = firstCardCommunity + 1;
+
+		while (secondCardCommunity != SIZE_OF_PLAYER_DECK)
+		{
+			combinationOfCards[2] = newCommunityCards[firstCardCommunity];
+			combinationOfCards[3] = newCommunityCards[secondCardCommunity];
+			combinationOfCards[4] = newCommunityCards[thirdCardCommunity];
+
+			if (isAStraigt(combinationOfCards)) {
+				return true;
+			}
+
+			thirdCardCommunity++;
+			if (thirdCardCommunity == SIZE_OF_COMMUNITY_CARDS)
+			{
+				secondCardCommunity++;
+				thirdCardCommunity = secondCardCommunity + 1;
+			}
+		}
+	}
+	return false;
+}
+
+void Straight::sortTheCards(Card**& vectorOfCombinations)
+{
+	for (int i = 0; i < SIZE_OF_PLAYER_DECK; i++)
+	{
+		for (int j = 0; j < SIZE_OF_PLAYER_DECK - i - 1; j++)
+		{
+			if (vectorOfCombinations[j]->getValue() > vectorOfCombinations[j + 1]->getValue())
+			{
+				Card* temp = vectorOfCombinations[j];
+				vectorOfCombinations[j] = vectorOfCombinations[j + 1];
+				vectorOfCombinations[j + 1] = temp;
+			}
+		}
+	}
+}
+
+bool Straight::isAStraigt(Card** vectorOfCombinations)
+{
+	ARRAY_OF_VALUES[0];
+	int index = 0;
+
+	while (vectorOfCombinations[0]->getValue() != ARRAY_OF_VALUES[index])
+	{
+		index++;
+	}
+
+	if (isACheckStraight(vectorOfCombinations, index))
+	{
+		return true;
+	}
+
+
+	return false;
+}
+
+bool Straight::isACheckStraight(Card** vectorOfCombinations, int startOfTheStaircase)
+{
+	int straightCounter = 0; 
+	for (int i = 0; i < COMMUNITY_CARD_SIZE; i++)
+	{
+		if (vectorOfCombinations[i]->getValue() == ARRAY_OF_VALUES[startOfTheStaircase])
+		{
+			straightCounter++; 
+		}
+		startOfTheStaircase++; 
+	}
+
+	if (straightCounter == COMMUNITY_CARD_SIZE)
+	{
+		return true;
+	}  
+	// Terminar lo de la condicion de la A, por que puede ser 1 o 14, por si sale una escalera de 10.
+
+	return false; 
+}
+
